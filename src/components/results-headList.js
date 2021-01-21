c08a865e7d05582ed5d75e884e54e1da6ae27f73
israelias <israelias.js@gmail.com>
2021-01-22 03:47:47 +0800

    Control head mount transitions at head-list

--- src/components/results-headList.js
+++ src/components/results-headList.js
@@ -1,15 +1,23 @@
 import React, {useState} from "react";
 import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Collapse from "@material-ui/core/Collapse"
 import List from '@material-ui/core/List';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import ResultsDefinitionsList from "./results-definitionsList"
-import HeaderContent from "./results-HeaderFeature"
+import Slide from '@material-ui/core/Slide';
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import {SameHeadShowTotal} from "../helpers/counters.helper"
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
         maxWidth: '100%',
         backgroundColor: 'inherit',
         position: 'relative',
+        [theme.breakpoints.up("sm")]: {
+            marginLeft: "8rem",
+        }
     },
     subheader: {
         backgroundColor: theme.palette.background.default,
@@ -18,48 +26,16 @@ const useStyles = makeStyles((theme) => ({
         height: '2em',
         fontSize: '20px',
     },
-    head: {
-        borderBottom: '1px solid'
-    },
     listSection: {
         backgroundColor: 'inherit'
-    },
-    subSection: {
-        backgroundColor: 'inherit',
-        maxWidth: 'max-width: calc(50% - 5px)',
-        flexWrap: 'wrap',
     },
     ul: {
         backgroundColor: 'inherit',
         padding: 0,
         position: 'relative',
-        // top: '3rem',
     },
-    nested: {
-        top: '3rem'
-    },
-    wordBoxSubHead: {
-        top: '3rem',
-        width: '100%',
-        display: 'flex',
-        alignItems: 'stretch',
-        flexDirection: 'column',
-        flex: '1',
-        paddingLeft: '16px',
-        boxSizing: 'border-box',
-    },
-    textSubHead: {
-        whitespace: 'nowrap',
-        textOverflow: 'ellipsis',
-        minHeight: 'calc(2em + 2px)',
-        display: 'flex',
-        alignItems: 'center',
-        flex: 1,
-        borderBottom: '1px solid',
-        paddingLeft: 16,
-
-        fontSize: '16px',
-        lineHeight: 1.2435,
+    icon: {
+        width: '100%'
     },
 }));
 
@@ -73,58 +49,69 @@ const ResultsHeadList = (props) => {
 
     return (
 
-        <List
-            component='ol'
-            className={classes.root}
-            id='results-head'>
-            {results.map((result) => (
-                <li
-                    key={`result-${result.meta.uuid}-${result.hwi.hw}`}
-                    className={classes.listSection}>
-                    <ul
-                        className={classes.ul}>
-                        <ListSubheader
-                            className={classes.subheader}>
-                            <HeaderContent
-                                key={
-                                    `head-${result.hwi.hw}`
-                                }
-                                root={
-                                    result.hwi.hw
-                                }
-                                uuid={
-                                    result.meta.target ?
-                                    result.meta.target.tuuid
-                                        : result.meta.uuid
-                                }
-                            />
-                        </ListSubheader>
-                        <li
-                            className={classes.listSection}>
-                            <ResultsDefinitionsList
-                                result={
-                                    result
-                                }
-                                headWord={
-                                    result.hwi.hw
-                                }
-                                headInstance={
-                                    result.meta.target ?
-                                        result.meta.target.tuuid
-                                        : result.meta.uuid
-                                }
-                                headLabel={
-                                    result.fl
-                                }
-                                onSelectionChange={
-                                    (value) => onSelectionChange(value)
-                                }
-                            />
-                        </li>
-                    </ul>
-                </li>
-            ))}
-        </List>
+        <Collapse
+            in={!loading}
+            // direction="up"
+            // mountOnEnter
+            // unmountOnExit
+               // style={{transitionDelay: '1500ms'}}
+        >
+            <List
+                component='ol'
+                className={classes.root}
+                id='results-head'>
+                {results.map((result) => (
+                    <li
+                        key={`result-${result.meta.uuid}-${result.hwi.hw}`}
+                        className={classes.listSection}>
+                        <ul
+                            className={classes.ul}>
+                            <ListSubheader
+                                className={classes.subheader}>
+                                <ListItemIcon className={classes.icon}>
+                                    <ListItemText
+                                        primary={
+                                            <span><strong>{result.hwi.hw}</strong>, <em>{result.fl}</em></span>
+                                        }
+                                    />
+                                    <SameHeadShowTotal
+                                        loading={loading}
+                                        root={result.hwi.hw}
+                                        label={result.fl}
+                                        uuid={result.meta.target ?
+                                            result.meta.target.tuuid
+                                            : result.meta.uuid}
+                                    />
+                                </ListItemIcon>
+                            </ListSubheader>
+                            <li
+                                className={classes.listSection}>
+                                <ResultsDefinitionsList
+                                    loading={loading}
+                                    result={
+                                        result
+                                    }
+                                    headWord={
+                                        result.hwi.hw
+                                    }
+                                    headInstance={
+                                        result.meta.target ?
+                                            result.meta.target.tuuid
+                                            : result.meta.uuid
+                                    }
+                                    headLabel={
+                                        result.fl
+                                    }
+                                    onSelectionChange={
+                                        (value) => onSelectionChange(value)
+                                    }
+                                />
+                            </li>
+                        </ul>
+                    </li>
+                ))}
+            </List>
+        </Collapse>
     );
 }
 
