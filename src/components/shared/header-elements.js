4f82b0149df46ee14b207fd31222ac330c373f19
israelias <israelias.js@gmail.com>
2021-01-27 15:21:53 +0800

    Develop shared header component to accommodate two header types

--- src/components/shared/header-elements.js
+++ src/components/shared/header-elements.js
@@ -1,13 +1,45 @@
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import Fade from "@material-ui/core/Grow";
+import Slide from "@material-ui/core/Slide";
 import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Box from "@material-ui/core/Box";
+
+
+const useStyles = makeStyles((theme) => ({
+    header: {
+        zIndex: 1200,
+        position: 'fixed',
+        top: 0,
+        display: 'flex',
+        width: '100%',
+        paddingRight: '0.8rem',
+        paddingLeft: '0.8rem',
+        paddingTop: '0.8rem',
+        backgroundColor: theme.palette.background.default,
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        '@media (min-width:600px)': {
+            alignItems: 'unset',
+        },
+    },
+}));
 
 const HeaderElements = ({props, children}) => {
+    const classes = useStyles()
     const trigger = useScrollTrigger();
     return (
-        <Fade appear={true} direction="left" in={!trigger}>
-            <div>{children}</div>
-        </Fade>
+        <Slide appear={true} direction="down" in={!trigger} >
+            <Box
+                component="header"
+                className={classes.header}>
+
+                    {children}
+
+                <Toolbar/>
+            </Box>
+        </Slide>
     );
 }
 
