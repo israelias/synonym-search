07fd8f7b1f9001d255c6275d4a645e58a4d8ef25
israelias <israelias.js@gmail.com>
2021-01-27 15:50:10 +0800

    Revise API call to always use cancellable helper function

--- src/services/mwThesaurusService.js
+++ src/services/mwThesaurusService.js
@@ -12,32 +12,14 @@ function searchThesaurus(searchText, selection) {
     const query = selection ? selection : searchText;
     const key = 'c212ac04-42cc-42dd-91fa-635bef08991c';
 
-    // const fetcher = url => fetch(url).then(res => res.json());
-    // const { data, error } = useSWR("/api/thesaurus", fetcher);
-    return axios.get(`thesaurus/json/${query}?key=${key}`, axiosConfig)
+    // return axios.get(`thesaurus/json/${query}?key=${key}`, axiosConfig)
 
-    // if (isServer()) {
-        // const { word } = axios.get(`${query}?key=${key}`, axiosConfig)
-        //     .then((response) => {
-        //         word.id = response.data[0].meta.uuid;
-        //         word.type = response.data[0].fl;
-        //         word.syns = response.data[0].meta.syns[0];
-        //         word.defs = response.data[0].shortdef;
-        //         word.complete = response.data[0].def[0].sseq[0];
-        // })
-        // return axios.get(`thesaurus/json/${query}?key=${key}`, axiosConfig);
-    // }
-
-    // return axiosGetCancellable(`/api/thesaurus/${query}?key=${key}`);
+    return axiosGetCancellable(`/thesaurus/json/${query}?key=${key}`, axiosConfig);
 
     // if (isServer()) {
     //     return axios.get(`${query}?key=${key}`, axiosConfig);
     // }
     // return axiosGetCancellable(`api/thesaurus/json/${query}?key=${key}`);
-}
-
-function getDefinition(word) {
-    return axios.get(`repositories/${id}`, axiosConfig);
 }
 
 // evaluates to true if we're running on server
