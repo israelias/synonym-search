23b46775ce2b2503a0cc3adadccf741c2e8d6e88
israelias <israelias.js@gmail.com>
2021-01-29 04:47:16 +0800

    Add favicon package

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,9 +19,15 @@ export default function MyApp(props) {
     return (
         <React.Fragment>
             <Head>
-                <title>Synonym Chaser</title>
+                <title>Synonym Search</title>
                 <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
-                <link rel="icon" href="/favicon.ico"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff0000" />
+                <meta name="msapplication-TileColor" content="#00a300" />
+                <meta name="theme-color" content="#ffffff" />
             </Head>
             <ThemeContextProvider>
                 {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
