911882279aed5c3aeef44312598a9165421175f7
israelias <israelias.js@gmail.com>
2021-02-02 09:32:07 +0800

    Add forked scroll-bottom component

--- src/components/shared/theme-switch.js
+++ src/components/shared/theme-switch.js
@@ -4,14 +4,17 @@ import Fab from '@material-ui/core/Fab';
 import { makeStyles } from '@material-ui/core/styles'
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import Slide from "@material-ui/core/Slide";
+import FixedBottom from "./fixed-bottom"
 import TonalityIcon from '@material-ui/icons/Tonality';
 import Avatar from "@material-ui/core/Avatar";
+
+const reactFixedBottom = require("react-fixed-bottom")
 
 const useStyles = makeStyles((theme) => ({
     fab: {
         position: 'fixed',
         zIndex: 1200,
-        bottom: theme.spacing(2),
+        bottom: 'offset',
         right: theme.spacing(2),
         [theme.breakpoints.up("sm")]: {
             left: theme.spacing(2),
@@ -33,20 +36,24 @@ const ToggleTheme = () => {
 
     return (
         <Slide appear={true} direction={"up"} in={!trigger}>
-            <Fab
-                size="small"
-                color="secondary"
-                aria-label="switch"
-                className={classes.fab}
-                onClick={handleClick}
-            >
-                <Avatar
-                    className={classes.switch}
-                    component={TonalityIcon}
-                    variant='circular'
-                    color='secondary'
-                />
-            </Fab>
+            <FixedBottom offset={16}>
+
+                <Fab
+                    size="small"
+                    color="secondary"
+                    aria-label="switch"
+                    className={classes.fab}
+                    onClick={handleClick}
+                >
+                    <Avatar
+                        className={classes.switch}
+                        component={TonalityIcon}
+                        variant='circular'
+                        color='secondary'
+                    />
+                </Fab>
+
+            </FixedBottom>
         </Slide>
     )
 }
