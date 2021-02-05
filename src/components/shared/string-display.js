e314197018f12a51c6c1e26f45a9b85bd336e18b
israelias <israelias.js@gmail.com>
2021-02-06 07:29:25 +0800

    Add comments to display string

--- src/components/shared/string-display.js
+++ src/components/shared/string-display.js
@@ -1,41 +1,48 @@
 import React from 'react'
-import { makeStyles } from "@material-ui/core/styles";
 import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
+import { useStyles } from "../../styles/display.styles"
 import { ReplaceSubStringNode } from "../../helpers/string.helper"
 import PropTypes from "prop-types";
 
-const useStyles = makeStyles(theme => ({
-    active: {
-        textDecoration: "none",
-        backgroundColor: theme.palette.action.hover,
-        fontWeight: 400,
-        color: theme.palette.primary.light,
-        transitionProperty: 'width',
-        transitionDelay: '100',
-    },
-    display: {
-        fontWeight: '300',
-        lineHeight: '1.1rem',
-        color: theme.palette.text.primary,
-        paddingTop: '.8rem',
-        paddingBottom: '.8rem',
-        marginLeft: '16px',
-    },
-    iconLeft: {
-        color: theme.palette.primary.light,
-        fontSize: "small",
-        marginLeft: '-16px',
-        marginRight: '16px',
-        borderBottom: '1px solid',
-        transform: 'scale(-1, 1)'
-    },
-    iconRight: {
-        color: theme.palette.primary.light,
-        fontSize: "small",
-    },
-}));
+/**
+ * Helper functions to display a clean string.
+ *
+ * Sense-definition objects often contain a verbal illustration
+ * (example sentence) to illustrate how a word is used in context. The
+ * outer-context string may or may not be wrapped in node brackets
+ * {lquo}like so{rquo}, and the inner-word string are always wrapped
+ * {it}like so{/it}, making it fairly straight-forward to re-illustrate
+ * the string dynamically with composed regex functions.
+ *
+ * @see   Display
+ * @since  1.12.21
+ * @file   defines ReplaceNodeTags, ReplaceSubStringNode
+ */
+
+/**
+ * Object which contains intent and related information.
+ * @typedef {Object} Display
+ * @property {string} sampleString - An example sentence featuring a Word.
+ * @property {string} optionWord - On of the synonymous terms interchangeable with the Word.
+ * @return {object}
+ */
 
 
+/**
+ * Component for showing a sense-definition's verbal illustration
+ * (example sentence) to illustrate how a word is used in context.
+ *
+ * @see   ReplaceSubStringNode
+ * @since  1.8.21
+ * @component
+ * @example
+ * const age = 21
+ * const sampleString = 'Getting the {it}foo{/it} out of here.'
+ * const optionWord = 'bar'
+ * return (
+ *   <Display sampleString={sampleString} optionWord={optionWord} />
+ * )
+ */
 const Display = ({
                      sampleString,
                      optionWord,
@@ -67,7 +74,13 @@ const Display = ({
 }
 
 Display.propTypes = {
+    /**
+     * An example sentence featuring a Word.
+     */
     sampleString: PropTypes.string.isRequired,
+    /**
+     * On of the synonymous terms interchangeable with the Word.
+     */
     optionWord: PropTypes.string.isRequired,
 };
 
